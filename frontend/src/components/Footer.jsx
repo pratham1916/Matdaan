c59c9192c493f71c22913e1a8e3c40c709a10764
pratham1916 <120631770+pratham1916@users.noreply.github.com>
2023-06-04 20:38:11 +0530

    Voting

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,21 @@
+import React from "react";
+import '../css/Footer.css'
+
 const Footer = () =>{
     return(
-        <>
-        Footer
-        </>
+    <>
+        <footer id="footer">
+            <div class="container">
+                <h3>मतदान !</h3>
+                <p>चाहे नर हो या हो नारी,मतदान है सबकी जिम्मेदारी।</p>
+                <div class="social-links">
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                </div>
+            </div>
+        </footer>
+    </>
     )
 }
 export default Footer;  
