410824d90bd2b58c89db06c63550554965b8192b
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-04-07 13:10:57 +0530

    SignIn oage added

--- frontend/src/AllRoutes/AllRoutes.jsx
+++ frontend/src/AllRoutes/AllRoutes.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import {Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Vote from '../pages/Vote';
 import Result from '../pages/Result';
 import Contact from '../pages/Contact';
 import PrivateRoute from './PrivateRoutes';
+import Login from '../pages/Login';
+import SignInPage from '../pages/SignInPage';
 
 const AllRoutes = () => {
     return (
         <div>
             <Routes>
                 <Route path='/' element={<Home />}></Route>
-                <Route path='/about' element={<About/>}></Route>
-                <Route path='/vote' element={<PrivateRoute><Vote/></PrivateRoute>}></Route>
-                <Route path='/result' element={<PrivateRoute><Result/></PrivateRoute>}></Route>
-                <Route path='/contact' element={<Contact/>}></Route>
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/vote' element={<PrivateRoute><Vote /></PrivateRoute>}></Route>
+                <Route path='/result' element={<PrivateRoute><Result /></PrivateRoute>}></Route>
+                <Route path='/contact' element={<Contact />}></Route>
+                <Route path='/SignIn' element={<SignInPage />}></Route>
             </Routes>
         </div>
     )
